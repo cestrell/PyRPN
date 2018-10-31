91711b8ed9b821c467df0776c22c0a099c9ed8cd
cestrell <cestrell@umich.edu>
2018-10-31 17:10:14 -0400

    Implemented multiplication

--- rpn.py
+++ rpn.py
@@ -17,8 +17,11 @@ def calculate(arg):
 				result = val1 - val2
 			elif token == '/':
 				result = val1 - val2
-			stack.append(result)
+			elif token == '*':
+				result = val1 * val2
 
+			stack.append(result)
+			
 	if len(stack) > 1:
 		raise ValueError('Too many arguments on the stack')
 
