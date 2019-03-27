81cb547962785bd0f379a52ac62752a821bc4766
Carlos Estrella <cestrell@umich.edu>
2019-03-27 03:11:30 -0400

    Update test_rpn.py

--- test_rpn.py
+++ test_rpn.py
@@ -1,34 +1,23 @@
 import unittest
 import rpn
-import rpn_instr
 
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-		result1 = rpn.calculate("1 1 +")
-		result2 = rpn_instr.calculate("1 1 +")
-		self.assertEqual(2, result1)
-		self.assertEqual(2, result2)
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(2, result)
 
 	def test_sub(self):
-		result1 = rpn.calculate("4 3 -")
-		result2 = rpn_instr.calculate("4 3 -")
-		self.assertEqual(1, result1)
-		self.assertEqual(1, result2)
+		result = rpn.calculate("4 3 -")
+		self.assertEqual(1, result)
 
 	def test_div(self):
-		result1 = rpn.calculate("4 2 /")
-		result2 = rpn_instr.calculate("4 2 /")
-		self.assertEqual(2, result1)
-		self.assertEqual(2, result2)
+		result = rpn.calculate("4 2 /")
+		self.assertEqual(2, result)
 	
 	def test_mul(self):
-		result1 = rpn.calculate("3 4 *")
-		result2 = rpn_instr.calculate("3 4 *")
+		result = rpn.calculate("3 4 *")
 		self.assertEqual(12, result1)
-		self.assertEqual(12, result2)
 
 	def test_exp(self):
-		result1 = rpn.calculate("2 3 ^")
-		result2 = rpn_instr.calculate("2 3 ^")
-		self.assertEqual(8, result1)
-		self.assertEqual(8, result2)
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result)
